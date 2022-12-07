03dab569062e7014ae309db78000b25a82b8dcbd
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-12-07 17:55:22 +0100

    chore: remove println in live logger

--- src/main/scala/it/unibo/util/LiveLogger.scala
+++ src/main/scala/it/unibo/util/LiveLogger.scala
@@ -3,7 +3,6 @@ package it.unibo.util
 import it.unibo.model.network.torch.log
 
 object LiveLogger {
-  println("Here..")
   private val writer = log.SummaryWriter()
   def logScalar(tag: String, value: Double, tick: Int): Unit = writer.add_scalar(tag, value, tick)
 }
